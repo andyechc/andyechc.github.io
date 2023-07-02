a897ba1a5b296dd48d38f0fb833e10c296a8c0bc
andev <andyechevarria0522@gmail.com>
2023-07-02 03:35:16 -0400

    update some files

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -7,9 +7,7 @@ export function Cover() {
   const { text } = useContext(LanguageContext);
 
   return (
-    <main
-      className={`bg-[url('bg-image.jpg')] bg-center bg-cover bg-no-repeat bg-gradient-to-b from-gray-900 from-30% to-zinc-950 flex flex-wrap-reverse justify-center items-center gap-10 px-10 md:px-0 py-20 shadow-[inset_0_60px_55px_-10px] shadow-gray-900 `}
-    >
+    <main className="bg-[url(assets/bg-image.jpg)] bg-center bg-cover bg-no-repeat bg-gradient-to-b from-gray-900 from-30% to-zinc-950 flex flex-wrap-reverse justify-center items-center gap-10 px-10 md:px-0 py-20 shadow-[inset_0_60px_55px] shadow-gray-900">
       <section className="flex flex-col gap-6 animate-show">
         <h2 className="text-4xl font-extrabold text-gray-100">
           J.Andy Echevarria Contreras
@@ -21,7 +19,7 @@ export function Cover() {
 
         <button className="w-[100px]">
           <a
-            className="block h-full py-1 bg-black bg-opacity-20 text-sm text-white font-extrabold border border-primary hover:bg-primary hover:text-gray-100 rounded-md transition-all"
+            className="block h-full py-1 text-sm text-white font-extrabold border border-primary hover:bg-primary hover:text-gray-100 rounded-md transition-all"
             href="#"
           >
             {text.cover.button}
