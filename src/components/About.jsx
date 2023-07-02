485689c2e0b04884576f0dd2f4a60ad65fef2858
andev <andyechevarria0522@gmail.com>
2023-07-02 17:04:58 -0400

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,6 +24,7 @@ export function About() {
         <div className="w-full flex flex-col gap-5 justify-center">
           <article className="text-left flex flex-col gap-5">
             <h4 className="text-xl md:text-2xl text-white font-bold">My Bio</h4>
+            <div className="flex flex-col gap-5 justify-center">
             <p className="text-gray-300 text-md md:text-lg font-light">
               Durante mis estudios universitarios en Ciencias Medicas, descubrí
               <b> mi interés por la programación</b> y decidí explorar más sobre
@@ -38,23 +39,32 @@ export function About() {
               en línea, tutoriales y proyectos personales. Aprendí lenguajes
               como <b className="text-yellow-500">Javascript</b> y frameworks
               relacionados a este para la construcción de sitios web. Termine
-              enamorado del ecosistema de <b className="text-sky-500">React</b>.
-              <b>
-                "La autodisciplina y la motivación han sido clave en mi éxito
-                como desarrollador autodidacta".
-              </b>
+              enamorado del ecosistema de <b className="text-sky-500">React</b>
             </p>
+
+            <b className="text-md text-white font-bold">
+              "La autodisciplina y la motivación han sido clave en mi éxito como
+              desarrollador autodidacta".
+            </b>
 
             <p className="text-gray-300 text-md md:text-lg font-light">
               A lo largo de mi aprendizaje, he enfrentado desafíos y obstáculos,
               pero siempre he mantenido mi pasión por aprender y mejorar mis
-              habilidades. Estoy constantemente buscando nuevas formas de
-              mejorar mi trabajo y estar al tanto de las últimas tendencias en
-              tecnología. Estoy siempre abierto a nuevas oportunidades y
-              desafíos en el campo de la programación. ¡Estoy emocionado de
-              seguir aprendiendo y creando soluciones innovadoras en el mundo
-              del desarrollo web!
+              habilidades.{" "}
+              <b>
+                Estoy constantemente buscando nuevas formas de mejorar mi
+                trabajo y estar al tanto de las últimas tendencias en
+                tecnología.
+              </b>{" "}
+              Estoy siempre abierto a nuevas oportunidades y desafíos en el
+              campo de la programación.
             </p>
+
+            <b className="text-md text-white font-bold">
+              ¡Estoy emocionado de seguir aprendiendo y creando soluciones
+              innovadoras en el mundo del desarrollo web!
+            </b>
+            </div>
           </article>
         </div>
       </article>
