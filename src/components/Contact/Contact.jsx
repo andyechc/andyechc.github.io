847cae99d4dba5216101a60560124258c64b7186
andev <andyechevarria0522@gmail.com>
2023-07-08 16:17:16 -0400

    create contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,10 +5,10 @@ export function Contact(){
     <section id='contact' className="w-full h-full py-32 px-10 bg-gradient-to-b from-black via-gray-950 via-70% to-zinc-950 flex flex-col justify-center items-center gap-24">
       <TitleSection title="Contact" quote="Keep In Touch"/>
       
-      <form className="w-full h-full max-w-[1000px] flex flex-col justify-center items-center gap-5 accent-primary text-white text-md font-normal caret-primary">
+      <form className="w-full h-full max-w-[800px] flex flex-col justify-center items-center gap-5 accent-primary text-white text-md font-normal caret-primary">
         <div className="w-full flex gap-5 justify-center items-center">
         <input 
-          className="w-full bg-transparent border border-gray-600 rounded px-2 py-3 focus:border-primary focus:placeholder-primary transition-all" 
+          className="w-full bg-transparent border border-gray-600 rounded px-2 py-3 focus:border-primary focus:outline-0 focus:placeholder-primary active:border-0 active:outline-0  transition-all" 
           type="text" 
           placeholder="Nombre"
         />
@@ -19,6 +19,12 @@ export function Contact(){
           placeholder="Apellidos"
         />
         </div>
+        
+        <input 
+          className="w-full bg-transparent border border-gray-600 rounded px-2 py-3 focus:border-primary focus:placeholder-primary transition-all" 
+          type="email" 
+          placeholder="Email"
+        />
         
         <textarea 
           className="w-full h-full bg-transparent border border-gray-600 rounded px-2 py-3 resize-y focus:border-primary focus:placeholder-primary transition-all" 
