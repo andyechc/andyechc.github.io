ac431ab59f7b99019dd148d0a7a2c9ded31300e7
andev <andyechevarria0522@gmail.com>
2023-07-15 17:51:40 -0400

    fix performance on page loading

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import {VitePWA} from 'vite-plugin-pwa';
+import{ VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,26 +8,26 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: 'auto',
       
       manifest: {
         name: "andev | Portfolio",
         short_name: "andev",
         description: "J.Andy Echevarria Contreras. Front-End Developer. A Developer Who Loves To Code.",
         theme_color: '#001120',
-        background_color: '#001120',
         
         icons: [
           {
             src: "/and-icon.svg",
-            type: "image/svg+xml",
             sizes:"512x512",
+            type: "image/svg+xml",
             purpose: "any maskable"
           },
           
           {
             src: "/and-icon.svg",
-            type: "image/svg+xml",
             sizes:"192x192",
+            type: "image/svg+xml",
             purpose: "any maskable"
           },
         ]
