ecd2322cd4e87ee920faab94105e7081dbadaa65
andev <andyechevarria0522@gmail.com>
2023-07-15 16:24:11 -0400

    fix performance on page loading

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 export function Logo() {
   return (
-    <h2 className="font-extrabold text-xl text-gray-100 flex items-center select-none cursor-default inline">
+    <h2 className="font-extrabold text-xl text-gray-100 flex gap-2 items-center select-none cursor-default inline">
       <img src="/and-icon.svg"/>
       andev
     </h2>
