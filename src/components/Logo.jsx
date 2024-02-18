371e7b48d7465bb9af7f4cd10d8bdd276fb02129
José Andy Echevarría Contreras <andyechevarria0522@gmail.com>
2024-02-17 20:29:20 -0500

    Delete old files

--- src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-export function Logo() {
-  return (
-    <h2 className="font-extrabold text-xl text-gray-100 flex gap-2 items-center select-none cursor-default inline">
-      <img src="/and-icon.svg"/>
-      andev
-    </h2>
-  );
-}
--- src/icons/vite.jsx
+++
@@ -1,7 +0,0 @@
-import vite from '/vite.svg'
-
-export default function Vite() {
-  return ( 
-    < img className = "w-14 h-14" src={vite} / >
-  );
-}
