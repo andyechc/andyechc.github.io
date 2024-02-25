f4369aedc63bdbdc5bc357a32aa132465fe8682e
José Andy Echevarría Contreras <andyechevarria0522@gmail.com>
2024-02-24 20:19:31 -0500

    change theme icons

--- script.js
+++ script.js
@@ -15,20 +15,20 @@ copy.innerText = "© " + year
 const theme = localStorage.getItem("theme")
 if (theme === 'dark') {
   document.body.classList.toggle('dark')
-  themeButton.src = "./assets/Icons/moon.svg"
+  themeButton.src = "./assets/Icons/sun.svg"
 }
 
 // Theme button functionality 
 themeButton.addEventListener('click', ()=>{
   if (document.body.className === "dark") {
     // Light theme actions
-    themeButton.src = "./assets/Icons/sun.svg"
+    themeButton.src = "./assets/Icons/moon.svg"
     markyImg.src = "./assets/Projects/marky.png"
     rmImg.src = "./assets/Projects/rm.png"
     localStorage.setItem("theme", "light")
   } else {
     // Dark theme actions
-    themeButton.src = "./assets/Icons/moon.svg"
+    themeButton.src = "./assets/Icons/sun.svg"
     markyImg.src = "./assets/Projects/marky-dark.png"
     rmImg.src = "./assets/Projects/rm-dark.png"
     localStorage.setItem("theme", "dark")
